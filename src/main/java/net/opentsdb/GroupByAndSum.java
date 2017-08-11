158f6007832e26e91beecb203f6c72cd9c643d60
Chris Larsen <clarsen@yahoo-inc.com>
2017-08-11 12:01:30 -0700

    JDoc

--- src/main/java/net/opentsdb/GroupByAndSum.java
+++ src/main/java/net/opentsdb/GroupByAndSum.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2017 Chris Larsen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.opentsdb;
 
 import static java.util.stream.Collectors.groupingBy;
@@ -10,14 +25,12 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
@@ -31,6 +44,12 @@ import net.opentsdb.data.TimeSeriesId;
 import net.opentsdb.utils.Bytes;
 import net.opentsdb.utils.Bytes.ByteMap;
 
+/**
+ * This mimics a read from OpenTSDB that pulls {@code TIMESERIES} with blocks of
+ * data for 1 day of values at 60 second intervals. Then we group by a particular
+ * tag and sum the data points at each timestamp. Assumes timestamps are nicely
+ * aligned.
+ */
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
 public class GroupByAndSum {
