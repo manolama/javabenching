4a207b00e4ebb36840f006b3b978b1c0199c7ccf
Chris Larsen <clarsen@yahoo-inc.com>
2017-08-16 16:53:14 -0700

    Add a List implementation for the SIMD test.

--- src/main/java/net/opentsdb/SimDTest.java
+++ src/main/java/net/opentsdb/SimDTest.java
@@ -1,5 +1,6 @@
 package net.opentsdb;
 
+import java.util.List;
 import java.util.Random;
 
 import org.openjdk.jmh.annotations.Benchmark;
@@ -10,6 +11,8 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
+
+import com.google.common.collect.Lists;
 
 /**
  * Benching SIMD, etc from Piotr's blog: http://prestodb.rocks/code/simd/
@@ -26,23 +29,66 @@ public class SimDTest {
   {
       public final int[] values = new int[SIZE];
       public final int[] results = new int[SIZE];
+      
+      public final double[] dvalues = new double[SIZE];
+      public final double[] dresults = new double[SIZE];
+      
+      public final List<Integer> lvalues = Lists.newArrayListWithCapacity(SIZE);
+      public final List<Integer> lresults = Lists.newArrayListWithCapacity(SIZE);
 
+      public final List<Double> ldvalues = Lists.newArrayListWithCapacity(SIZE);
+      public final List<Double> ldresults = Lists.newArrayListWithCapacity(SIZE);
+      
       @Setup
       public void setup()
       {
           Random random = new Random();
           for (int i = 0; i < SIZE; i++) {
               values[i] = random.nextInt(Integer.MAX_VALUE / 32);
+              lvalues.add(values[i]);
+              dvalues[i] = random.nextDouble();
+              ldvalues.add(dvalues[i]);
           }
       }
   }
   
   @Benchmark
-  public int[] increment(Context context)
+  public int[] myIntegerArrayIncrement(Context context)
   {
       for (int i = 0; i < SIZE; i++) {
           context.results[i] = context.values[i] + 1;
       }
       return context.results;
+  }
+  
+  @Benchmark
+  public double[] myDoubleArrayIncrement(Context context)
+  {
+      for (int i = 0; i < SIZE; i++) {
+          context.dresults[i] = context.dvalues[i] + 1;
+      }
+      return context.dresults;
+  }
+  
+  @Benchmark
+  public List<Integer> myIntegerListIncrement(Context context)
+  {
+      // sucks
+      context.lresults.clear();
+      for (int i : context.lvalues) {
+          context.lresults.add(i + 1);
+      }
+      return context.lresults;
+  }
+  
+  @Benchmark
+  public List<Double> myDoubleListIncrement(Context context)
+  {
+      // sucks
+    context.ldresults.clear();
+      for (double i : context.ldvalues) {
+          context.ldresults.add(i + 1);
+      }
+      return context.ldresults;
   }
 }
